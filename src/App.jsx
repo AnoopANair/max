5e4e0cacb583d3fe2a1a56ffd64a8ee852607818
Anoop A Nair <mailtoanoop71@gmail.com>
2024-07-28 17:34:22 +0000

    removed slideshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Cards from "./components/Cards/Cards"
 import Navbar from "./components/NavBar/Navbar"
-import Slideshow from "./components/Slideshow/Slideshow"
 function App() {
   return(
     <>
     
     <Navbar/>
-    <Slideshow/>
     <Cards/>
     </>
   )
