3196a2b0056af685e52b943e28524da08d82aacf
Anoop A Nair <mailtoanoop71@gmail.com>
2024-07-31 02:18:06 +0000

    added flex properly to cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 function Cards(){
     return(
         <>
-        <div style={{display:"flex"}}>
+        <div style={{display:"flex",justifyContent:"center", flexWrap:"wrap", border: "1px solid black"}}>
             <Card/>
             <Card/>
             <Card/>
